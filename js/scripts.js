60572b7384ba9e03e92379594b899820ad801339
Jacob Hixon and Skye Atwood <jacob@>
2016-11-02 11:00:49 -0700

    add scorekeeping logic and turn change logic, display players scores

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,22 @@ function randomize(min, max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+var currentTurn = "player1";
+
+function changeTurn() {
+  if (currentTurn === "player1") {
+    currentTurn = "player2";
+  }
+  else if (currentTurn === "player2") {
+    currentTurn = "player1";
+  }
+}
+
+var turnScore = 0;
+
+var player1Score = 0;
+var player2Score = 0;
+
 var randomNumber = "";
 
 
@@ -13,8 +29,43 @@ var randomNumber = "";
 $(document).ready(function() {
   $("#roll-button").click(function(event) {
     event.preventDefault();
-    randomNumber = randomize(1, 6);
+    $("span#player-roll").empty();
 
-    if (randomNumber === 1)
-  })
-})
+    randomNumber = randomize(1, 6);
+    $("span#player-roll").append(randomNumber);
+
+    if (randomNumber === 1) {
+      turnScore = 0;
+      changeTurn();
+      alert("You rolled a 1! Next Player's turn!");
+    }
+    else if (randomNumber > 1) {
+      turnScore += randomNumber;
+      $("span#turn-score").empty();
+      $("span#turn-score").append(turnScore);
+
+
+    }
+  });
+  $("#pass-button").click(function(event) {
+    event.preventDefault();
+
+    if (currentTurn === "player1") {
+      player1Score += turnScore;
+      $("h4#player1-score").empty();
+      $("h4#player1-score").append(player1Score);
+      alert("Next Player's turn!");
+      changeTurn();
+      turnScore = 0;
+    }
+    else if (currentTurn === "player2") {
+      player2Score += turnScore;
+      $("h4#player2-score").empty();
+      $("h4#player2-score").append(player2Score);
+      alert("Next Player's turn!");
+      changeTurn();
+      turnScore = 0;
+    }
+  });
+
+});

e5fbe49b741f11fe9145b68ba73b5dc57fa589c9
Jacob Hixon and Skye Atwood <skye@>
2016-11-02 14:29:12 -0700

    Add style, flashing player scores.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// Business Logic //
 function randomize(min, max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
@@ -14,40 +14,40 @@ function changeTurn() {
   }
 }
 
+function clearTurnScore() {
+  $("span#player-roll").empty();
+  $("span#turn-score").empty();
+}
+
 function newGame() {
   $("span#player-roll").empty();
   $("span#turn-score").empty();
   alert("Game Over!")
   player1Score = 0;
   player2Score = 0;
-  $("h4#player1-score").empty();
-  $("h4#player1-score").append(player1Score);
-  $("h4#player2-score").empty();
-  $("h4#player2-score").append(player2Score);
+  $("h1#player1-score").empty();
+  $("h1#player1-score").append(player1Score);
+  $("h1#player2-score").empty();
+  $("h1#player2-score").append(player2Score);
 }
-
 var turnScore = 0;
-
 var player1Score = 0;
 var player2Score = 0;
-
 var randomNumber = "";
 
-
-
-
-
-
+// User Interface Logic //
 $(document).ready(function() {
   $("#roll-button").click(function(event) {
     event.preventDefault();
     $("span#player-roll").empty();
 
     randomNumber = randomize(1, 6);
+
     $("span#player-roll").append(randomNumber);
 
     if (randomNumber === 1) {
       turnScore = 0;
+      clearTurnScore();
       changeTurn();
       alert("You rolled a 1! Next Player's turn!");
     }
@@ -58,7 +58,6 @@ $(document).ready(function() {
       if (currentTurn === "player1" && turnScore + player1Score >= 100) {
         player1Score += turnScore
         alert("Player 1 Wins!");
-
         newGame();
       }
       else if (currentTurn === "player2" && turnScore + player2Score >= 100) {
@@ -66,7 +65,6 @@ $(document).ready(function() {
         alert("Player 2 Wins!");
         newGame();
         changeTurn();
-
       }
     }
   });
@@ -75,17 +73,35 @@ $(document).ready(function() {
 
     if (currentTurn === "player1") {
       player1Score += turnScore;
-      $("h4#player1-score").empty();
-      $("h4#player1-score").append(player1Score);
-      alert("Next Player's turn!");
+      $("h1#player1-score").addClass("red-score");
+      $("h1#player1-score").fadeOut();
+      $("h1#player1-score").empty();
+      $("h1#player1-score").append(player1Score);
+      $("h1#player1-score").fadeIn();
+      $("h1#player1-score").fadeOut();
+      $("h1#player1-score").fadeIn();
+      $("h1#player1-score").fadeOut();
+      $("h1#player1-score").fadeIn(function() {
+        $("h1#player1-score").removeClass("red-score");
+      });
+      clearTurnScore();
       changeTurn();
       turnScore = 0;
     }
     else if (currentTurn === "player2") {
       player2Score += turnScore;
-      $("h4#player2-score").empty();
-      $("h4#player2-score").append(player2Score);
-      alert("Next Player's turn!");
+      $("h1#player2-score").addClass("red-score");
+      $("h1#player2-score").fadeOut();
+      $("h1#player2-score").empty();
+      $("h1#player2-score").append(player2Score);
+      $("h1#player2-score").fadeIn();
+      $("h1#player2-score").fadeOut();
+      $("h1#player2-score").fadeIn();
+      $("h1#player2-score").fadeOut();
+      $("h1#player2-score").fadeIn(function() {
+        $("h1#player2-score").removeClass("red-score");
+      });
+      clearTurnScore();
       changeTurn();
       turnScore = 0;
     }
